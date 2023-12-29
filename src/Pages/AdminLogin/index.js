39a517e96038a62a0da343fdac7ae87a632fd630
kamal <kamallochanboruah7@gmail.com>
2023-12-29 16:22:06 +0530

    new commit

--- src/Pages/AdminLogin/index.js
+++ src/Pages/AdminLogin/index.js
@@ -1,44 +1,43 @@
-import Cookies from 'js-cookie';
+import React, { useState } from 'react';
 import AdminPanel from '../AdminPanel/adminPanel';
-import React, { useState, useEffect } from 'react';
 
 function AdminLogin() {
   const [authenticated, setAuthenticated] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
+  let popup;
 
-  useEffect(() => {
-    const token = Cookies.get('token');
-    const localStorageToken = localStorage.getItem('token');
-    if (token && token === localStorageToken) {
+  const receiveMessage = (event) => {
+    if (
+      event.origin === 'https://nestjs-backend-vilx.onrender.com' &&
+      event.data.token
+    ) {
+      document.cookie = `token=${event.data.token}; SameSite=None; Secure`;
+      localStorage.setItem('token', event.data.token);
       setAuthenticated(true);
-    } else {
-      console.log('Token mismatch or does not exist.');
-    }
-  }, []);
-
-  console.log(authenticated);
-
-  const handleGoogleLogin = () => {
-    try {
-      const popup = window.open(
-        'https://nestjs-backend-vilx.onrender.com/google',
-        'Google Auth',
-        'width=600,height=600'
-      );
-
-      window.addEventListener('message', (event) => {
-        if (event.origin === 'https://nestjs-backend-vilx.onrender.com' && event.data.token) {
-          // Set the 'token' cookie with the SameSite=None and Secure attributes
-          document.cookie = `token=${event.data.token}; SameSite=None; Secure`;
-          localStorage.setItem('token', event.data.token);
-          setAuthenticated(true);
-          popup.close();
-        }
-      });
-    } catch (error) {
-      console.error('Error logging in with Google:', error);
+      popup.close();
     }
   };
 
+  const handleGoogleLogin = () => {
+    if (clickCount === 0) {
+      // First click: open Google sign-in popup
+      try {
+        popup = window.open(
+          'https://nestjs-backend-vilx.onrender.com/google',
+          'Google Auth',
+          'width=600,height=600'
+        );
+
+        window.addEventListener('message', receiveMessage);
+      } catch (error) {
+        console.error('Error logging in with Google:', error);
+      }
+      setClickCount(1);
+    } else if (clickCount === 1) {
+      setAuthenticated(true)
+      setClickCount(0); 
+    }
+  };
   return (
     <div>
       {authenticated ? (
