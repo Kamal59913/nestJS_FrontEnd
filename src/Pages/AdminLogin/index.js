23f84056fa95e3a2d731fb20da6a7d1856d9e266
kamal <kamallochanboruah7@gmail.com>
2023-12-29 15:41:36 +0530

    new comment

--- src/Pages/AdminLogin/index.js
+++ src/Pages/AdminLogin/index.js
@@ -1,15 +1,13 @@
 import Cookies from 'js-cookie';
 import AdminPanel from '../AdminPanel/adminPanel';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function AdminLogin() {
   const [authenticated, setAuthenticated] = useState(false);
-  const popupRef = useRef(null);
 
   useEffect(() => {
     const token = Cookies.get('token');
     const localStorageToken = localStorage.getItem('token');
-
     if (token && token === localStorageToken) {
       setAuthenticated(true);
     } else {
@@ -21,34 +19,23 @@ function AdminLogin() {
 
   const handleGoogleLogin = () => {
     try {
-      // Open a popup window for Google authentication
       const popup = window.open(
         'https://nestjs-backend-vilx.onrender.com/google',
         'Google Auth',
         'width=600,height=600'
       );
-      setAuthenticated(true);
-      popupRef.current = popup;
-      retrieveToken();
+
+      window.addEventListener('message', (event) => {
+        if (event.origin === 'https://nestjs-backend-vilx.onrender.com' && event.data.token) {
+          // Set the 'token' cookie with the SameSite=None and Secure attributes
+          document.cookie = `token=${event.data.token}; SameSite=None; Secure`;
+          localStorage.setItem('token', event.data.token);
+          setAuthenticated(true);
+          popup.close();
+        }
+      });
     } catch (error) {
       console.error('Error logging in with Google:', error);
-    }
-  };
-
-  const retrieveToken = () => {
-    const token = Cookies.get('token');
-    if (token) {
-      // Store the token in localStorage for future use
-      localStorage.setItem('token', token);
-      setAuthenticated(true);
-      window.location.href = '/adminlogin'; 
-      if (popupRef.current) {
-        popupRef.current.close();
-      }
-
-      window.location.href = '/adminlogin'; 
-    } else {
-      console.log('Token cookie does not exist or is not accessible.');
     }
   };
 
