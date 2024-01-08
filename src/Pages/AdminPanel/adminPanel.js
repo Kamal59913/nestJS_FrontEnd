d6f481e6c0e676ea019779ac2793beddc7c4eb15
kamal <kamallochanboruah7@gmail.com>
2024-01-08 23:02:15 +0530

    new commit

--- src/Pages/AdminPanel/adminPanel.js
+++ src/Pages/AdminPanel/adminPanel.js
@@ -2,15 +2,22 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './admins.css';
 import { useNavigate } from 'react-router-dom';
+import { googleLogout } from '@react-oauth/google';
+
 
 export default function AdminPanel() {
   const [users, setUsers] = useState([]);
   const [load, setLoad] = useState(false);
+  const [secure, setSecure] = useState(false);
   const [api, setApi] = useState('');
   const objectId = "658c1ac43b9561d7d764dddb";
   const navigate = useNavigate();
 
   useEffect(() => {
+    console.log("this right here is",localStorage.getItem('authData'),"hi this is local storage");
+    if(localStorage.getItem('authData')){
+      setSecure(true);
+    }
     fetchUsers();
   }, []);
 
@@ -62,65 +69,76 @@ export default function AdminPanel() {
   };
 
   const handlelogout = async () => {
-    localStorage.removeItem('authData');
+    await googleLogout();
+    await localStorage.removeItem('authData');
+    await setSecure(false);
     navigate('/');
   };
 
   return (
     <>
-      <div>
-        <div className='left'>
-          <h1>Welcome to the Admin Panel</h1>
-          {load? (<>
-            <table className='table'>
-            <thead>
-              <tr>
-                <th>Chat Id</th>
-                <th>Location</th>
-                <th>Username</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user, userIndex) => (
-                <tr key={userIndex}>
-                  <td>{user.tele_id}</td>
-                  <td>{user.location}</td>
-                  <td>{user.username}</td>
-                  <td>
-                    <button className="btn btn-danger btn-sm" type='submit' onClick={() => deleteUser(user.tele_id)}>Remove</button>
-                    {user.isBlocked === true ? (
-                      <button className="btn btn-warning btn-sm" type='submit' onClick={() => blockUser(user.tele_id)}>Unblock</button>
-                    ) : (
-                      <button className="btn btn-warning btn-sm" onClick={() => blockUser(user.tele_id)}>Block</button>
-                    )}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+      {
+          secure ? 
+          (<>
+                <div>
+
+<div className='left'>
+  <h1>Welcome to the Admin Panel</h1>
+  {load? (<>
+    <table className='table'>
+    <thead>
+      <tr>
+        <th>Chat Id</th>
+        <th>Location</th>
+        <th>Username</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {users.map((user, userIndex) => (
+        <tr key={userIndex}>
+          <td>{user.tele_id}</td>
+          <td>{user.location}</td>
+          <td>{user.username}</td>
+          <td>
+            <button className="btn btn-danger btn-sm" type='submit' onClick={() => deleteUser(user.tele_id)}>Remove</button>
+            {user.isBlocked === true ? (
+              <button className="btn btn-warning btn-sm" type='submit' onClick={() => blockUser(user.tele_id)}>Unblock</button>
+            ) : (
+              <button className="btn btn-warning btn-sm" onClick={() => blockUser(user.tele_id)}>Block</button>
+            )}
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+  </>):(<>
+  <h1> Loading ... </h1>
+  </>)}
+</div>
+<div className='apiReplace'>
+  <form onSubmit={handleSubmit}>
+    <div className='txt_field'>
+      <input
+        type="text"
+        name="fname"
+        value={api}
+        onChange={(e) => setApi(e.target.value)}
+        required
+      />
+      <span></span>
+      <label>Change it with your own API keys</label>
+    </div>
+    <input type="submit" className="btn btn-light" value="Send" />
+  </form>
+</div>
+<input type="submit" className="button-logout" value="Admin Logout" onClick={handlelogout} />
+                </div>
           </>):(<>
-          <h1> Loading ... </h1>
-          </>)}
-        </div>
-        <div className='apiReplace'>
-          <form onSubmit={handleSubmit}>
-            <div className='txt_field'>
-              <input
-                type="text"
-                name="fname"
-                value={api}
-                onChange={(e) => setApi(e.target.value)}
-                required
-              />
-              <span></span>
-              <label>Change it with your own API keys</label>
-            </div>
-            <input type="submit" className="btn btn-light" value="Send" />
-          </form>
-        </div>
-        <input type="submit" className="button-logout" value="Admin Logout" onClick={handlelogout} />
-      </div>
+          <h6> Oops not authenticated to access this page </h6>
+          </>)
+        }
+
     </>
   );
 }
